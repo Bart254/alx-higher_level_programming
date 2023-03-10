accbd1fc2bccd9ee6eada3d266440073758e5c5a
Bart254 <omondibartlett3@gmail.com>
2023-03-10 15:29:59 +0300

    correct

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     import sys
     total_args = len(sys.argv)
     if total_args == 1:
-        print(f'{total_args} argument.')
+        print(f'{total_args - 1} arguments.')
+    elif total_args == 2:
+        print(f'{total_args - 1} argument:')
+        print("1: {}".format(sys.argv[1]))
     else:
         print(f'{total_args - 1} arguments:')
         for e in range(total_args):
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     hidden_list = dir(hidden_4)
-    for e in range(len(hidden_list)):
-        if hidden_list[e][0] != '_' and hidden_list[e][1] != '_':
-            print(f'{hidden_list[e]')
+    for name in hidden_list:
+        if name[0] == '_' and name[1] == '_':
+            continue
+        print(name)
