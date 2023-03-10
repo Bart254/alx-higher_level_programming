43db694b4b04f191eb9fa0420411c4080122287e
Bart254 <omondibartlett3@gmail.com>
2023-03-10 16:58:51 +0300

    modify

--- 0x02-python-import_modules/100-write.py
+++ 0x02-python-import_modules/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+    from sys import stderr
+    stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
     sys.exit(1)
