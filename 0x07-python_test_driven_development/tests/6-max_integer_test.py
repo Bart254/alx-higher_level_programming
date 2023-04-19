f464e6f69b5c0b5f000a90bf7cde4357e4fddbac
Bart254 <omondibartlett3@gmail.com>
2023-04-19 20:25:29 +0300

    add test files

---
+++ 0x07-python_test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Unittest for max_integer([..])
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TextMaxInteger(unittest.TestCase):
+    """ subclass of testcase
+    """
+    def test_max(self):
+        """ asserts the accuracy of a function checking the largest
+        integer of a function
+        """
+        self.assertEqual(max_integer([1, 2, 4, 6, 8]), 8)
+        self.assertEqual(max_integer([-1, -2, 0]), 0)
+        self.assertIs(max_integer([]), None)
+
+if __name__ == "__main__":
+    unittest.main()
