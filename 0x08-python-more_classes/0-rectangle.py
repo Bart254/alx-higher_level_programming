e75df71c5d4ec1afcf4d01c1652910718e5cfa0f
Bart254 <113597942+Bart254@users.noreply.github.com>
2023-07-04 08:25:00 +0300

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module that creates a rectangle class"""
-
-
-class Rectangle:
-    """An empty class defining a rectangle
-    """
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-This is a deeper dive into OOP introduction. Additional concepts handled are Inheritance and Polymorphism
