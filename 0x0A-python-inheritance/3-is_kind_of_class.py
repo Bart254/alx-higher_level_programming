ac95a18f84a9ea75c40c35c3e4b45c3b851ee97a
Bart254 <113597942+Bart254@users.noreply.github.com>
2023-07-04 08:24:35 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" chess if object is an instance of a class"""
-
-
-def is_kind_of_class(obj, a_class):
-    """ returns true or false"""
-    return True if isinstance(obj, a_class) else False
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Empty class"""
-    pass
