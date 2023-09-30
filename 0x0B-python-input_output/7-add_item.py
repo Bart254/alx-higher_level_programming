89d9ad669c4846e5e97157ffc08a5dcf22e85321
Bart254 <omondibartlett3@gmail.com>
2023-10-01 01:06:57 +0300

    add function 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script adds all arguments to a Python list, and then save them to a file
+"""
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+try:
+    exist_d = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    exist_d = []
+for arg in sys.argv[1:]:
+    exist_d.append(arg)
+
+save_to_json_file(exist_d, "add_item.json")
