4285a5fde4b5de147443581508d92f866004c617
Bart254 <omondibartlett@gmail.com>
2023-12-14 11:48:40 +0300

    update file 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let total = 0;
   for (let index = 0; list[index]; index++) {
     if (list[index] === searchElement) {
