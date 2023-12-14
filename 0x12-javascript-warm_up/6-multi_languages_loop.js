aca71c13ea0730036ccc1cd496860da5e46b793f
Bart254 <omondibartlett@gmail.com>
2023-12-14 10:05:12 +0300

    update file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; lines[index]; index++) {
   console.log(lines[index]);
 }
