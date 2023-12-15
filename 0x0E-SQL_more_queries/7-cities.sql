91ddafd433955c988fd4a591514f4bee6609fe77
Bart254 <omondibartlett@gmail.com>
2023-12-15 17:52:15 +0300

    update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
--- Creates database
+
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Use the database
 USE `hbtn_0d_usa`;
 -- creates a table in database
 CREATE TABLE IF NOT EXISTS `cities` (
-	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY,
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
 	`state_id` INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES state(id),
+	FOREIGN KEY (state_id) REFERENCES states(id),
 	`name` VARCHAR(256) NOT NULL );
