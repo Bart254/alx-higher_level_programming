8852a987f3429bbd7b13d60f2228e48b51eba5c6
Bart254 <omondibartlett@gmail.com>
2023-12-15 17:21:31 +0300

    remove use statement

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,5 @@
 -- Creates database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
--- Use the database
-USE `hbtn_0d_usa`;
 -- creates a table in database
 CREATE TABLE IF NOT EXISTS `states` (
 	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY,
