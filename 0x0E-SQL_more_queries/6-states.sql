2333d6484825ca6020831b98078bbb90d8ab7627
Bart254 <omondibartlett@gmail.com>
2023-12-15 17:28:54 +0300

    change constraint primary to primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Creates database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- use the database
+USE `hbtn_0d_usa`;
 -- creates a table in database
 CREATE TABLE IF NOT EXISTS `states` (
-	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY,
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
 	`name` VARCHAR(256) NOT NULL );
